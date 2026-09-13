ca1f3d20192e54106608f738f83cf7f18a790ddd
„Nikita <nikita.rysiev@gmail.com>
2026-09-13 22:39:52 +0200

    make build

---
+++ index.a306f828.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");e&&e.querySelectorAll("tr").forEach(function(e){var r=e.children;if(r.length>=2){var l=r[1],t=r[r.length-1],n=l.cloneNode(!0);e.insertBefore(n,t)}});
+//# sourceMappingURL=index.a306f828.js.map
